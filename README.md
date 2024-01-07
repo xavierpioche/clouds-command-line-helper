a5daaea6e604efe6ff27f4a7b3a038846baa6b21
xavierpioche <xavier.pioche@laposte.net>
2024-01-07 18:15:56 -0400

    readme completion and j2parse correcting

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# clouds-command-line-helper
-a set of common command lines to use aws, gcp, azu and oci
+# Clouds-command-line-helper
+
+A set of common command lines to use aws, gcp, azu and oci formatted in jinja2 templates. It is possible to generate linux ou windows like multiline commands.
+
+## Installation 
+
+In the "scopes" directory, there are the file for variables values in yaml format. You should put different files with your own naming convention according to your projects. 
+
+## Usage
+
+To generate cloud command lines, it's just with the short script j2render.py:
+
+./j2rendy.py -s gcp-myproject-variables.yaml -t gcp-vpc-create.j2 -o gcp-myproject-vpc-create.sh 
+
+for example, the gcp-myproject-variables.yaml in the scope directory:
+```yaml
+variablescopes:
+- { VmName: instance-1,
+    VmProject: oca-infra-xp-lab,
+    VmOsImage: rhel-8,
+    VmOsImageProject: rhel-cloud,
+    VmMachineType: e2-medium,
+    VmZoneLocation: europe-west9-a,
+    VmVpcName: lab-test-project,
+    VmVpcSubnet: back,
+    VmIsPublic: false,
+    VmIsShielded: true,
+    isCodeisLinux: false
+  }
+```
+## Contributing
+
+You are welcome to add some template files with common command used in order to jumpstart projects or labs quickly.  
--- j2render.py
+++ j2render.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import sys
 import os.path 
 from jinja2 import Environment, FileSystemLoader, Template
@@ -10,10 +11,10 @@ try:
 except ImportError:
     from yaml import Loader, Dumper
 
-parser = argparse.ArgumentParser(description="example : -s myproject/project.yaml -t gcp-vm-create.j2 -o myproject/gcp-vm-create.bat", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("-s", "--scope", help="yaml variables file for scope")
-parser.add_argument("-t", "--template", help="template file to render")
-parser.add_argument("-o", "--output", help="output file to write")
+parser = argparse.ArgumentParser(description="example : -s project.yaml -t gcp-vm-create.j2 -o gcp-vm-create.bat", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-s", "--scope", help="yaml variables file for scope in scope dir")
+parser.add_argument("-t", "--template", help="template file to render in template dir")
+parser.add_argument("-o", "--output", help="output file to write in output dir")
 args = parser.parse_args()
 
 if not len(sys.argv) == 7:
@@ -24,6 +25,8 @@ if not len(sys.argv) == 7:
 config = vars(args)
 
 TemplatesDirectory="templates/"
+ScopesDirectory="scopes/"
+OutputDirectory="output/"
 
 ENV = Environment(loader=FileSystemLoader(TemplatesDirectory))
 
@@ -31,10 +34,10 @@ if not os.path.isfile(config['scope']) or not os.path.isfile(TemplatesDirectory+
     print("some input files are missing")
     sys.exit()
 
-with open(config['scope']) as inputfile:
+with open(ScopesDirectory+config['scope']) as inputfile:
     variablescopes =  yaml.load(inputfile, Loader=Loader)
 
 template = ENV.get_template(config['template'])
 
-with open(config['output'], "w") as file:
+with open(OutputDirectory+config['output'], "w") as file:
     file.write(template.render(variablescopes=variablescopes))
